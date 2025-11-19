3fe82afe3044f0e5103c52c39384e8d1e07dac7b
DATDQ1510 <datdq5275@gmail.com>
2025-11-19 13:57:41 +0700

    update redis connection

--- Backend/src/config/redis.ts
+++ Backend/src/config/redis.ts
@@ -1,14 +1,17 @@
 import dotenv from "dotenv";
-import Redis from "ioredis/built/Redis";
+import Redis from "ioredis";
 
 dotenv.config();
+console.log("Redis Host:", process.env.REDIS_HOST);
+console.log("Redis Port:", process.env.REDIS_PORT);
 const redisConnection = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
+  host: process.env.REDIS_HOST,  // từ env, ví dụ red-d4eke8fpm1nc738t3sj0
   port: Number(process.env.REDIS_PORT) || 6379,
-  maxRetriesPerRequest: null
+  password: process.env.REDIS_PASSWORD || undefined, // nếu Render yêu cầu
+  maxRetriesPerRequest: null,
 });
+
 redisConnection.on("connect", () => console.log("✅ Redis connected"));
 redisConnection.on("error", (err) => console.error("❌ Redis error:", err));
-
 
 export default redisConnection;
